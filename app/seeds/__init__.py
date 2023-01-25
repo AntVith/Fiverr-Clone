d36cb7178e3484ac49af90f20ee7fd5ac56a7a71
Anthony <avithayathil23@gmail.com>
2023-01-25 11:31:56 -0500

    seeded correctly now

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,5 +1,8 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
+from .reviews import seed_reviews, undo_reviews
+from .bookings import seed_bookings, undo_bookings
+from .services import seed_services, undo_services
 
 from app.models.db import db, environment, SCHEMA
 
@@ -12,17 +15,26 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     if environment == 'production':
-        # Before seeding in production, you want to run the seed undo 
-        # command, which will  truncate all tables prefixed with 
+        # Before seeding in production, you want to run the seed undo
+        # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
+        undo_reviews()
+        undo_bookings()
+        undo_services()
         undo_users()
     seed_users()
+    seed_services()
+    seed_bookings()
+    seed_reviews()
     # Add other seed functions here
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
+    undo_reviews()
+    undo_bookings()
+    undo_services()
     undo_users()
     # Add other undo functions here
