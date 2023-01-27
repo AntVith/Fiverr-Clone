e214a862498b73a41512b67ac0a8af39ee8e17cf
Anthony <avithayathil23@gmail.com>
2023-01-27 13:18:05 -0500

    edit modal styled

--- react-app/src/components/EditServiceModal/index.js
+++ react-app/src/components/EditServiceModal/index.js
@@ -4,7 +4,7 @@ import { useHistory } from "react-router-dom";
 import { useModal } from "../../context/Modal";
 import { useSelector } from "react-redux";
 import {editAService} from '../../store/service'
-
+import './editService.css'
 
 function EditService(serviceId){
     const sessionUser = useSelector(state => state.session.user);
@@ -54,52 +54,72 @@ function EditService(serviceId){
     return (
         <div id='UploadServiceContainer'>
             <form id='upload-service-form' onSubmit={handleSubmit} method="post">
+            <h2 id='post-title'>Edit your service</h2>
                 <ul>
                     {errors.map((error, idx) => (
                       <li key={idx}>{error}</li>
                      ))}
                  </ul>
 
-                <input
-                type='text'
-                required
-                onChange={(e) => setTitle(e.target.value)}
-                placeholder='Title'
-                name='title'
-                value={title}
-                />
-                <textarea
-                type='text'
-                required
-                onChange={(e) => setDescription(e.target.value)}
-                placeholder='Description'
-                name='description'
-                value={description}
-                />
-                <input
-                type='number'
-                pattern="[0-9]*"
-                required
-                onChange={(e) => setPrice(e.target.value)}
-                placeholder='$1'
-                name='price'
-                value={price}
-                />
-                <input
-                type='text'
-                required
-                onChange={(e) => setThumbnail(e.target.value)}
-                placeholder='Thumbnail'
-                name='thumbnail'
-                value={thumbnail}
-                />
-                <button className='song-submit-button' type="submit"
-                // disabled={isLoading}
-                >Upload</button>
-                {showSuccess && (
-                <div>
-                    <p>Post Successful!</p>
+                <div id='title-post'>
+                    <label className='post-labels'>Title</label>
+                    <input
+                        type='text'
+                        required
+                        className='Post-Service-Inputs'
+                        onChange={(e) => setTitle(e.target.value)}
+                        placeholder='Title'
+                        name='title'
+                        value={title}
+                        />
                 </div>
+                <div id='description-post'>
+                    <label className='post-labels'>Description</label>
+                        <textarea
+                        type='text'
+                        required
+                        className='Post-Service-Inputs'
+                        onChange={(e) => setDescription(e.target.value)}
+                        placeholder='Description'
+                        name='description'
+                        value={description}
+                        />
+                </div>
+                <div id='price-post'>
+                    <label className='post-labels'>Price</label>
+                        <input
+                        type='number'
+                        pattern="[0-9]*"
+                        required
+                        className='Post-Service-Inputs'
+                        onChange={(e) => setPrice(e.target.value)}
+                        placeholder='$1'
+                        name='price'
+                        value={price}
+                        />
+                </div>
+                <div id='thumbnail-post'>
+                    <label className='post-labels'>Thumbnail Image</label>
+                        <input
+                        type='text'
+                        required
+                        className='Post-Service-Inputs'
+                        onChange={(e) => setThumbnail(e.target.value)}
+                        placeholder='Thumbnail'
+                        name='thumbnail'
+                        value={thumbnail}
+                        />
+                </div>
+                        <button
+                        className='Post-Service-Inputs'
+                         type="submit"
+                         id='submit-post'
+                        // disabled={isLoading}
+                        >Upload</button>
+                        {showSuccess && (
+                        <div>
+                            <p>Post Successful!</p>
+                        </div>
 
                 )}
             </form>
