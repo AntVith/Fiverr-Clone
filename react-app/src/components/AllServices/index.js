1bda55162babcdb0acb125d6c246b4edf50e3597
Anthony <avithayathil23@gmail.com>
2023-01-28 18:48:36 -0500

    added banner

--- react-app/src/components/AllServices/index.js
+++ react-app/src/components/AllServices/index.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import {getAllServices} from '../../store/service'
-// import { TransitionGroup, CSSTransition } from 'react-transition-group'
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
 import './allServices.css'
 function AllServices() {
     const dispatch = useDispatch()
@@ -10,6 +10,7 @@ function AllServices() {
     const services = Object.values(servicesObj)
     const totalServices = services.length
     const [users, setUsers] = useState([]);
+    const [currentIndex, setCurrentIndex] = useState(0);
     useEffect(() => {
        dispatch(getAllServices())
        async function fetchData() {
@@ -19,6 +20,15 @@ function AllServices() {
       }
       fetchData();
     }, [dispatch])
+
+    const images = ['https://www.freecodecamp.org/news/content/images/2021/01/image-155.png','https://kinsta.com/wp-content/uploads/2021/03/javascript-libraries.png', 'https://miro.medium.com/max/1400/1*3vOvKvhf_YC5XzU6oFWRhw.png', 'https://www.makewonder.com/wp-content/uploads/sites/4/2021/01/coding-languages-image.jpeg', 'https://res.cloudinary.com/practicaldev/image/fetch/s--YHR2L-mf--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://dev-to-uploads.s3.amazonaws.com/i/68cjjc8ujhz1o6anuq6s.png', 'https://www.indiewire.com/wp-content/uploads/2017/10/matrix-code.jpg']
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+          setCurrentIndex((currentIndex + 1) % images.length);
+        }, 5000);
+        return () => clearInterval(interval);
+    }, [currentIndex]);
 
 
 
@@ -43,8 +53,18 @@ function AllServices() {
 
 
     return (
-        <div>
-        <div id='banner-section'>hello</div>
+        <div id='whole-home-page'>
+        <div id='imgSlider'>
+        <TransitionGroup>
+          <CSSTransition
+          key={images[currentIndex]}
+          timeout={8000}
+          classNames='slide'
+          >
+          <img id='splash-image-login'  src={images[currentIndex]}/>
+          </CSSTransition>
+        </TransitionGroup>
+      </div>
         <div id='service-container'>
             <div id='all-services'>
             {services.map(service => (
