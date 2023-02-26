2d408d1481257cec3b70cf847af90fa5428edf1d
Anthony <avithayathil23@gmail.com>
2023-02-26 13:08:50 -0500

    review route in commit

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify , request
 from flask_login import login_required
 from app.models import  Review, db
-# from ..forms.booking_form import ReviewForm
+from ..forms.review_form import ReviewForm
 
 review_routes = Blueprint('review', __name__)
 
@@ -11,3 +11,21 @@ def get_reviews_of_booking(serviceId):
     reviews = Review.query.filter(Review.service_id == serviceId)
 
     return {'reviews': [review.to_dict() for review in reviews]}
+
+# post a review of a service
+@review_routes.route('/new', methods=['POST'])
+def post_review():
+    form = ReviewForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+
+    if form.validate_on_submit():
+        new_review = Review()
+        form.populate_obj(new_review)
+
+        db.session.add(new_review)
+        db.session.commit()
+        return new_review.to_dict(), 201
+    else:
+        return {
+            "errors": form.errors
+        }, 400
